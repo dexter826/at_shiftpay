f08d24d164c9a51aa7736999030c74a151394b15
dexter826 <2001222641@hufi.edu.vn>
2025-12-23 00:11:57 +0700

    feat(Loading): Add full-screen overlay and center loader for improved user experience

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -206,6 +206,17 @@ const Loader = () => {
 };
 
 const StyledWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 9999;
+  background-color: rgba(0, 0, 0, 0.3);
+
   .loader {
     width: fit-content;
     height: fit-content;
